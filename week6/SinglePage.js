0f8ac6c28f1eb1cc44aed1bf0d04c0055156f692
DevonGW <58575960+DevonGW@users.noreply.github.com>
2020-09-16 23:43:07 -0400

    Add files via upload

---
+++ week6/SinglePage.js
@@ -0,0 +1,25 @@
+async function getData(){
+    try{
+        const name = await axios.get("https://rickandmortyapi.com/api/character/1/")
+        const status = await axios.get("https://rickandmortyapi.com/api/character/1/")
+        const species = await axios.get("https://rickandmortyapi.com/api/character/1/")
+             listPlanetsToDOM(name, status, species)
+    }   
+    catch(error){
+        console.log(error)
+    }
+}
+getData()
+function listPlanetsToDOM(name, status, species) {
+    const p = document.createElement('p')
+    p.textContent = name.data.name
+    document.body.appendChild(p)
+
+    const p2 = document.createElement('p')
+    p2.textContent = status.data.status
+    document.body.appendChild(p2)
+
+    const p3 = document.createElement('p')
+    p3.textContent = species.data.species
+    document.body.appendChild(p3)
+}
